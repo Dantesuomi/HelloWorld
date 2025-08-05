9a2e1cc2045b84b246231f97186cf83902a987c4
Suvorova <yevheniia.suvorova@accenture.com>
2025-08-05 13:21:14 +0300

    changed Jenkins file to add test run

--- src/test/java/com/test/demo/DemoApplicationTests.java
+++ src/test/java/com/test/demo/DemoApplicationTests.java
@@ -1,13 +1,42 @@
 package com.test.demo;
 
+import com.test.demo.Service.GreetService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	private final GreetService greetService = new GreetService();
+
 	@Test
 	void contextLoads() {
 	}
+	@Test
+	public void testGreetPersonWithName() {
+		String result = greetService.greetPerson(Optional.of("Alice"));
+		assertEquals("Hello, Alice", result);
+	}
+
+	@Test
+	public void testGreetPersonWithoutName() {
+		String result = greetService.greetPerson(Optional.empty());
+		assertEquals("Hello World", result);
+	}
+
+	@Test
+	public void testGreetPersonWithBlankName() {
+		String result = greetService.greetPerson(Optional.of(""));
+		assertEquals("Hello, ", result);
+	}
+
+
+
+
+
 
 }
